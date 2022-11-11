6ad1aa6d5adb96ba767d004ee8389643320192dc
Rafat <rafatansari12feb1999@gmail.com>
2022-11-11 20:50:31 +0530

    Import Contacts-App folders content

---
+++ Contacts-App/React-Fundamentals-C1-React-Contacts/src/components/App.js
@@ -0,0 +1,37 @@
+import "../css/App.css";
+import ListContacts from "./ListContacts";
+
+const App = () => {
+
+  const contacts = [
+    {
+      id: "karen",
+      name: "Karen Isgrigg",
+      handle: "karen_isgrigg",
+      avatarURL: "http://localhost:5001/karen.jpg",
+    },
+    {
+      id: "richard",
+      name: "Richard Kalehoff",
+      handle: "richardkalehoff",
+      avatarURL: "http://localhost:5001/richard.jpg",
+    },
+    {
+      id: "tyler",
+      name: "Tyler McGinnis",
+      handle: "tylermcginnis",
+      avatarURL: "http://localhost:5001/tyler.jpg",
+    },
+  ];
+
+  console.log("haha");
+
+  return (
+    <div>
+      <ListContacts contactsList={contacts}/>
+    </div>
+  );
+
+};
+
+export default App;
---
+++ Contacts-App/React-Fundamentals-C1-React-Contacts/src/components/ListContacts.js
@@ -0,0 +1,20 @@
+const ListContacts = ({ contactsList }) => {
+    
+    console.log(contactsList);
+
+    return <ol className="contact-list">
+            {
+                contactsList.map((contact) => 
+                <li key={contact.id} className="contact-list-item">
+                    <div className="contact-avatar" style={{backgroundImage:`url(${contact.avatarURL})`}}></div>
+                    <div className="contact-details">
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button className="contact-remove"></button>
+                </li>)
+            }
+        </ol>
+};
+
+export default ListContacts;
