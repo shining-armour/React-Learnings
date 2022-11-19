a6427ece94b527bf45fe36f649cd03ea6828edb6
Rafat <rafatansari12feb1999@gmail.com>
2022-11-19 22:38:04 +0530

    Add Contact functionality, useNavigate()

--- Contacts-App/React-Fundamentals-C1-React-Contacts/src/components/AddContact.js
+++ Contacts-App/React-Fundamentals-C1-React-Contacts/src/components/AddContact.js
@@ -1,5 +1,32 @@
-const AddContact = () => {
-    return <p>Add Contact</p>
+import {Link} from "react-router-dom" 
+import ImageInput from "./ImageInput"
+import serializeForm from "form-serialize"
+import PropTypes from "prop-types"
+
+const AddContact = ({onAddContact}) => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const values = serializeForm(e.target, {hash:true})
+
+        onAddContact(values)
+    }
+    return (<div>
+        <Link to={"/"} className="close-create-contact">Close</Link>
+        <form className="create-contact-form" onSubmit={handleSubmit}>
+            <ImageInput className="create-contact-avatar-input" maxHeight={64} name="avatarURL" />
+            <div className="create-contact-details">
+                <input name="name" placeholder="Name" type="text"/>
+                <input name="handle" placeholder="Handle" type="text"/>
+                <button>Add Contact</button>
+            </div>
+        </form>
+        </div>);
+}
+
+AddContact.propTypes = {
+    onAddContact: PropTypes.func.isRequired
 }
 
 export default AddContact;
--- Contacts-App/React-Fundamentals-C1-React-Contacts/src/components/App.js
+++ Contacts-App/React-Fundamentals-C1-React-Contacts/src/components/App.js
@@ -1,11 +1,13 @@
 import "../css/App.css";
 import { useState, useEffect} from "react";
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import ListContacts from "./ListContacts";
 import * as ContactsAPI from"../utils/ContactsAPI"
 import AddContact from "./AddContact";
 
 const App = () => {
+
+  let navigate = useNavigate();
 
   const removeContact = (contactToBeRemoved) => {
     // removes contact from API
@@ -17,6 +19,22 @@ const App = () => {
 
   // props should be avoided in useState 
   const [contacts, setContacts] = useState([])
+
+  const addContact = (contactToBeAdded) => {
+
+    const add = async() => {
+      const result = await ContactsAPI.create(contactToBeAdded) // object with id is returned as result.
+      console.log("Result :: " + JSON.stringify(result))  
+      setContacts([...contacts, result]) // OR setContacts(contacts.concat(result))
+      console.log(contacts)
+    }
+    add()
+    // navigate back to ListContacts after adding contact
+    navigate("/")
+    // Don't do this - object with id is needed for list item.
+    // ContactsAPI.create(contactToBeAdded)
+    // setContacts([...contacts, contactToBeAdded])
+  }
 
 
 
@@ -36,7 +54,7 @@ const App = () => {
   return (
     <Routes>
       <Route exact path="/" element={<ListContacts contactsList={contacts} onDeleteContact={removeContact} />}/>
-      <Route path="/add" element={<AddContact/>}/>
+      <Route path="/add" element={<AddContact onAddContact={addContact}/>}/>
     </Routes>
   );
 
